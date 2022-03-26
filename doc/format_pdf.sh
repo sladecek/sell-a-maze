eb5191dad581bf1a419ea204f114fe8e90d170fc
Ladislav Sládeček <ladislav.sladecek@gmail.com>
2022-03-26 13:26:26 +0100

    Empty documentation

---
+++ doc/format_pdf.sh
@@ -0,0 +1,8 @@
+pandoc report.md  --toc --pdf-engine=xelatex -o ../static/report.pdf \
+       --number-sections \
+       -V colorlinks=true \
+       -V linkcolor=blue \
+       -V urlcolor=red \
+       -V toccolor=gray
+
+
---
+++ doc/report.md
@@ -0,0 +1,9 @@
+% Proving Existence of Maze Solutions in Zero Knowledge
+% v0.1 2022-03-27
+
+# Changelog
+
+## v0.1 2022-03-27
+
+Initial version
+
